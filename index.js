49ae22e268abd1b4494725aa420255b7121d36bc
Daniel Hallada <hallada.daniel@gmail.com>
2019-05-14 11:29:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function fetchBooks() {
-
+	return fetch('https://anapioficeandfire.com/api/books')
+	.then(resp => resp.json())
+	.then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('body')
+  debugger
   json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
